87e1ad0d6252c736dd1b13aff75aab2a0bd54553
ykrop142 <ykrop142@gmail.com>
2020-07-18 16:36:50 +0300

    laravel not change

--- bootstrap/cache/services.php
+++ bootstrap/cache/services.php
@@ -32,10 +32,11 @@
     28 => 'Carbon\\Laravel\\ServiceProvider',
     29 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     30 => 'Barryvdh\\Debugbar\\ServiceProvider',
-    31 => 'App\\Providers\\AppServiceProvider',
-    32 => 'App\\Providers\\AuthServiceProvider',
-    33 => 'App\\Providers\\EventServiceProvider',
-    34 => 'App\\Providers\\RouteServiceProvider',
+    31 => 'Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
+    32 => 'App\\Providers\\AppServiceProvider',
+    33 => 'App\\Providers\\AuthServiceProvider',
+    34 => 'App\\Providers\\EventServiceProvider',
+    35 => 'App\\Providers\\RouteServiceProvider',
   ),
   'eager' => 
   array (
@@ -57,10 +58,11 @@
     15 => 'Carbon\\Laravel\\ServiceProvider',
     16 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     17 => 'Barryvdh\\Debugbar\\ServiceProvider',
-    18 => 'App\\Providers\\AppServiceProvider',
-    19 => 'App\\Providers\\AuthServiceProvider',
-    20 => 'App\\Providers\\EventServiceProvider',
-    21 => 'App\\Providers\\RouteServiceProvider',
+    18 => 'Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
+    19 => 'App\\Providers\\AppServiceProvider',
+    20 => 'App\\Providers\\AuthServiceProvider',
+    21 => 'App\\Providers\\EventServiceProvider',
+    22 => 'App\\Providers\\RouteServiceProvider',
   ),
   'deferred' => 
   array (
--- resources/views/testbd.blade.php
+++ resources/views/testbd.blade.php
@@ -5,16 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>glav</title>
+    <title>123</title>
 </head>
 <body>
-<ul>
-    <li>
-        <a href="/contact">cont</a>
-    </li>
-    <li>
-        <a href="/testbd">bd</a>
-    </li>
-</ul>
+<?php
+
+if(DB::connection()->getDatabaseName())
+{
+   echo "Yes! successfully connected to the DB: " . DB::connection()->getDatabaseName();
+}else{ echo 'неа';}
+?>
+
 </body>
 </html>
