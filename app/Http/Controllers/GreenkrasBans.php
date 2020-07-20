3b9ba0f7ffa934692bb97ca623cbf9664e9c5b77
ykrop142 <ykrop142@gmail.com>
2020-07-20 10:16:10 +0300

    бан бд

--- app/Http/Controllers/GreenkrasBans.php
+++ app/Http/Controllers/GreenkrasBans.php
@@ -7,9 +7,23 @@ use App\Bans;
 
 class GreenkrasBans extends Controller
 {
+
     public function viewers(){
         $ban = Bans::all();
-     //   return $ban;
         return view('admin.bans',compact('ban'));
+    }
+
+    public function create(){
+        return view('admin.create');
+    }
+
+    public function storenewban(){
+        $ban = new Bans();
+        $ban->id_user=request('id_user');
+        $ban->reason=request('reason');
+        $ban->validity=request('validity');
+        $ban-> save();
+        return redirect('/admin');
+        //return request()->all();
     }
 }
