4ddc3d301ca72a7fbd23ac40fb54763a42d27f21
ykrop142 <ykrop142@gmail.com>
2020-07-26 13:45:25 +0300

    меню

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,8 @@
+
+@yield('content')
+    <ul>
+        <li><a href="/">Главная</a></li>
+        <li><a href="/admin">Банлист</a></li>
+        <li><a href="/admin/create">Банхамер</a></li>
+    </ul>
+
