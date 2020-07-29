70903edcebe4a755189582499a0ad8a8b1af8426
ykrop142 <ykrop142@gmail.com>
2020-07-29 09:59:04 +0300

    инфа в таблицу + немного стилей + кнопка под дел

--- resources/views/admin/bans.blade.php
+++ resources/views/admin/bans.blade.php
@@ -10,37 +10,75 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <title>Банлист</title>
+    <style>
+        .test1 {
+            width: 70%; /* Ширина блока */
+            padding: 10px; /* Поля */
+            margin-top: 10%; /* Отступ сверху */
+            margin-left: 10%; /* Отступ сверху */
+            border: 2px solid #000; /* Параметры рамки */
+            -moz-box-sizing: border-box; /* Для Firefox */
+            box-sizing: border-box; /* Ширина блока с полями */
+        }
+        #test2{
+            position: absolute;
+            top: 10%;
+            left: 50%;
+        }
+        .btnban {
+            background: #ea2626;
+            border: 0;
+            border-right: 1px solid #cbcbcc;
+            height: 100%;
+            width: 100%;
+        }
+        .btnunban {
+            background: #16862a;
+            border: 0;
+            border-right: 1px solid #cbcbcc;
+            height: 100%;
+            width: 100%;
+        }
+        .btn:hover {
+            background: rgba(105, 125, 219, 0.95);
+        }
+    </style>
 </head>
 <body>
 @extends('layout')
 @section('content')
-    <p>Банлист</p>
-    <ul>
-        @foreach($ban as $bans)
-            <h3>
-                <li>
-                    {{ $bans->id_user }}
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editbanModal{{$bans->id}}">
-                        Изменить причину/время блокировки
-                    </button>
-                    <ul>
-                        <li>
-                            {{ $bans->reason }}
-                        </li>
-                        <ul>
-                            <li>
-                                {{$bans->validity}}
-                            </li>
-                        </ul>
-                    </ul>
+    <p ><h2 id="test2">Банлист</h2></p>
+@endsection
 
-                </li>
-            </h3>
+    <table bordercolor="black" border="1" class="test1" width="100%">
+        @foreach($ban as $bans)
+            <tr>
+                <h3>
+                    <td>
+                        {{ $bans->id_user }}
+                    </td>
+                    <td>
+                        {{ $bans->reason }}
+                    </td>
+                    <td>
+                        {{$bans->validity}}
+                    </td>
+                    <td>
+                        <button type="button" class="btnban btn-primary" data-toggle="modal" data-target="#editbanModal{{$bans->id}}">
+                            Редактировать
+                        </button>
+                        <button type="button" class="btnunban btn-primary" data-toggle="modal" data-target="#editbanModal{{$bans->id}}">
+                            Разблокировать
+                        </button>
+                    </td>
+                </h3>
+            </tr>
+
             <div class="modal fade" id="editbanModal{{$bans->id}}" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="ModalLabel">BANHAMMER</h5>
+                            <h5 class="modal-title" id="ModalLabel">Редактор бана</h5>
                         </div>
                         <div class="modal-body">
                             <form action="/admin/{{$bans->id}}" method="post">
@@ -65,7 +103,7 @@
                 </div>
             </div>
         @endforeach
-    </ul>
-@endsection
+</table>
+
 </body>
 </html>
