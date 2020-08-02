18e5ab7d47572db220e16ddaf861c7a7af7f1c4b
ykrop142 <ykrop142@gmail.com>
2020-08-02 12:38:19 +0300

    user table

--- database/migrations/2020_08_02_090350_create_users_g_k_table.php
+++ database/migrations/2020_08_02_090350_create_users_g_k_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUsersGKTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,12 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->timestamp('email_verified_at');
+            $table->string('login');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('email');
+            $table->string('token');
+            $table->integer('id_tit');
+            $table->integer('exp');
+            $table->string('avatar');
+            $table->boolean('auth');
+            $table->date('created_at');
         });
     }
 
