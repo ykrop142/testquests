1769491598bdc2893495f9e212a7778e8bec992e
ykrop142 <ykrop142@gmail.com>
2020-08-04 13:14:07 +0300

    css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,12 @@
-
+<style>
+    #menu{
+        position: absolute;
+        top: 1%;
+        left: 1%;
+    }
+</style>
 @yield('content')
-    <ul>
+    <ul id="menu">
         <li><a href="/">Главная</a></li>
         <li><a href="/admin">Банлист</a></li>
         <li><a href="/admin/create">Банхамер</a></li>
