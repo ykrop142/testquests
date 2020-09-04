b2fc4534a1267a1231925e65e682b2aa618fc5bb
ykrop142 <ykrop142@gmail.com>
2020-09-04 21:35:14 +0300

    fix layout css + html

---
+++ resources/views/main.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('title', 'Главная страница')
+
+@section('sidebar')
+    @parent
+@endsection
+
+@section('namestr', 'Главная страница')
+
+@section('content')
+    <div>Регистрация</div>
+    <div>Авторизация</div>
+@endsection
