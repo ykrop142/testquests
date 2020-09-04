2b0f5c75ad3589d8a9e4d69947173761b4e7b0db
ykrop142 <ykrop142@gmail.com>
2020-09-04 21:34:47 +0300

    fix group rout

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('main', function () {
+    return view('main');
+});
 
 Route::group(['prefix'=>'admin'],function (){
     Route::get('users','GreenKrasBPost@indexuser');
@@ -23,6 +26,8 @@ Route::group(['prefix'=>'admin'],function (){
         return view('admin.panel');
     });
     Route::get('/','GreenKrasBPost@index');
+    Route::get('/create','GreenKrasBPost@create');
+    Route::post('/create','GreenKrasBPost@store');
     Route::patch('/{id}','GreenKrasBPost@update');
     Route::delete('/{id}','GreenKrasBPost@destroy');
    // Route::resource('','GreenKrasBPost');
