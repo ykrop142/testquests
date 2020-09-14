25cf0be42285cb1ceb0a0b4b1ce33abbc4a91e7d
ykrop142 <ykrop142@gmail.com>
2020-09-14 14:13:30 +0300

    update

--- app/Users.php
+++ app/Users.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Users extends Model
 {
-    //
+    public $timestamps = true;
+    const UPDATED_AT = null;
+    protected  $primaryKey = 'login';
+    public $incrementing = false;
 }
