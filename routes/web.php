eebd677a7f8c66215f681f243f12158ea7c79ae2
ykrop142 <ykrop142@gmail.com>
2020-09-14 14:12:19 +0300

    reg + middle checkreg

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::get('main', function () {
     return view('main');
 });
 
+Route::get('reg','GKUserAdd@create');
+Route::post('reg','GKUserAdd@store')->middleware('checkreg');;
+
 Route::group(['prefix'=>'admin'],function (){
     Route::get('users','GreenKrasBPost@indexuser');
     Route::get('panel', function () {
