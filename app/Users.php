79900d0217d6b8310280d357fe0f584999068dcb
ykrop142 <ykrop142@gmail.com>
2020-09-22 14:24:00 +0300

    install npm + auth (edit model)

--- app/Users.php
+++ app/Users.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Users extends Model
+class Users extends Authenticatable
+
 {
     public $timestamps = true;
     const UPDATED_AT = null;
+    protected $fillable = ['login','email','password'];
     //protected  $primaryKey = 'login';
     //public $incrementing = false;
 }
