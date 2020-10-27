fd0bcd793ebeefa111596e1b68fa443badf6f3e4
ykrop142 <ykrop142@gmail.com>
2020-10-27 21:59:52 +0300

    редактирование меню (пользователь)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,6 +10,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/d5f712159e.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset("css/bootstrap.min.css") }}">
     <link rel="stylesheet" href="{{ asset("css/dropmain.css") }}">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
@@ -47,33 +48,35 @@
                 </details>
 
             </li>
-            <li>
-                <details>
-                    <summary class="textcolors">Личный кабинет</summary>
-                    <summary> <a href="#">Личный кабинет</a></summary>
-                    <summary><a href="#">Профиль</a></summary>
-                    <summary><a href="#">Статистика</a></summary>
-                    <summary><a href="#">Настройки</a></summary>
-                    <summary>
+            <li class="dropdown" id="liucp">
+                <a href="javascript:void(0);" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img class="userAvatar" src="https://thumbs.dreamstime.com/z/123-%D0%BD%D0%BE%D0%BC%D0%B5%D1%80%D0%B0-%D0%BF%D0%BB%D0%B0%D1%81%D1%82%D0%B8%D1%87%D0%BD%D0%BE%D0%B3%D0%BE-11904974.jpg" alt=""><span class="hidden-xs">username</span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="/profile"><i class="fas fa-user"></i>Профиль</a></li>
+                    <li><a href="/ucp"><i class="fas fa-cog"></i>Настройки</a></li>
+                    <li><a href="/ucp"><i class="fas fa-bell"></i>Голосования</a></li>
+                    <li class="divider"></li>
+                    <li>
                         <?php
-                        if (Auth::check()) {
+                            if (Auth::check()) {
                         ?>
                         <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">{{ __('Выйти') }}</a>
+                            document.getElementById('logout-form').submit();"><i class="fas fa-power-off"></i>{{ __('Выйти') }}</a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                             @csrf
                         </form>
                         <?php
-                        }
-                        else
-                        {
+                            }
+                            else
+                            {
                         ?>
-                        <a href="{{ route('login') }}">{{ __('Войти') }}</a>
+                        <a href="{{ route('login') }}"><i class="fas fa-power-off"></i>{{ __('Войти') }}</a>
                         <?php
-                        }
+                            }
                         ?>
-                    </summary>
-                </details>
+                    </li>
+                </ul>
             </li>
         </ul>
     </nav>
