aaa40759945642f52f8a4b131b8783bb8fcab534
ykrop142 <ykrop142@gmail.com>
2020-10-28 11:14:51 +0300

    rename controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::MAIN;
 
     /**
      * Create a new controller instance.
