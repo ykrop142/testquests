21ee606d7a2b02e463dccb08dac5182adaded17b
ykrop142 <ykrop142@gmail.com>
2020-11-02 15:09:08 +0300

    профиль юзера

--- resources/views/lk/profile.blade.php
+++ resources/views/lk/profile.blade.php
@@ -1,3 +1,6 @@
+<head>
+    <link rel="stylesheet" href="{{ asset("css/profile.css") }}">
+</head>
 @extends('layout')
 <?php $login=Auth::user()->login; ?>
 @section('title', 'Профиль '. $login)
@@ -7,5 +10,25 @@
 @endsection
 
 @section('content')
-
+    <p> </p>
+    <p> </p>
+    <div class="container" style="z-index:1">
+        <div class="row">
+            <div class="col">
+                <p> <img class="userAvatar" src="{{Auth::user()->avatar}}" alt="" style="width: 200px;height: 200px"></p>
+                <p> " Прогресс бар " {{Auth::user()->exp}}</p>
+                <p> Ваш уровень : {{Auth::user()->lvl}}</p>
+                <p> Титул [ <font style="color:{{$titles->RGB}}">{{$titles->name}} </font>]</p>
+            </div>
+            <div class="col">
+                <p>Логин: {{Auth::user()->login}}</p>
+                <p>Почта: {{Auth::user()->email}}</p>
+                <p>Подтверждение почты: {{Auth::user()->auth}}</p>
+                <p>Жалоб: {{Auth::user()->varn}}</p>
+                <p>Отмечено объектов: в разработке</p>
+                <p>Всего голосов: в разработке</p>
+                <p>Регистрация аккаунта: {{Auth::user()->created_at}}</p>
+            </div>
+        </div>
+    </div>
 @endsection
