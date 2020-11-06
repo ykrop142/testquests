b3c1c99ed2a3fef7e90e2138d25269303ac18975
ykrop142 <ykrop142@gmail.com>
2020-11-06 13:31:38 +0300

    recent

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -8,7 +8,7 @@
 
 <body>
 @extends('layout')
-@section('title', 'Banhammer')
+@section('title', 'Блокировка пользователей')
 @section('sidebar')
     @parent
 @endsection
@@ -16,19 +16,6 @@
 @section('namestr', '')
 
 @section('content')
-
-<!-- Button ban modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#banModal">
-    Banhammer
-</button>
-<!-- Modal -->
-<div class="modal fade" id="banModal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="ModalLabel">BANHAMMER</h5>
-            </div>
-            <div class="modal-body">
                 <form action="/admin/create" method="post">
                     @csrf
                     <div>
@@ -40,15 +27,10 @@
                     <div>
                         <input type="date" required name="validity" placeholder="длительность" value="{{$dater}}"/>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
                         <button type="submit" class="btn btn-primary">Заблокировать</button>
                     </div>
                 </form>
-            </div>
-        </div>
-    </div>
-</div>
+
 @endsection
 </body>
 </html>
