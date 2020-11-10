bc86189ed312039389e52691180e2373992b2e82
ykrop142 <ykrop142@hmail.com>
2020-11-10 15:03:24 +0300

    del

---
+++ database/migrations/2020_11_10_075322_create__item__table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('nameitem');
+            $table->string('image');
+            $table->string('composition');
+            $table->string('description');
+            $table->string('type');
+            $table->integer('weight');
+            $table->string('image')->default('https://test.greenkras.ru/image/photoitem/susi.png');
+            $table->integer('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
