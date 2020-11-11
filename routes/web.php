d614f4a6456efbf65b0792295c80244eeb5aafc0
ykrop142 <ykrop142@hmail.com>
2020-11-11 18:01:56 +0300

    update route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', 'ItemC@index');
 Route::get('/view/{vibor}','ItemC@viewitems');
 Route::get('/about','About@index');
+Route::get('/contact','Contact@index');
+Route::post('/contact/feedback','Contact@feedback');
+Route::get('/alert','Contact@alert');
+Route::get('/orders','OrderC@index');
+Route::post('/orders/add/{id}','OrdC@add');
+Route::get('/orders/up/{id}','OrdC@up');
+Route::get('/orders/down/{id}','OrdC@down');
