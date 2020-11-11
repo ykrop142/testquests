12bbd0197bfed813894194b0d9190c78404b7502
ykrop142 <ykrop142@hmail.com>
2020-11-11 18:06:25 +0300

    + модели

---
+++ app/Ord.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ord extends Model
+{
+    public $table = "ord";
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
+}
